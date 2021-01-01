6ce0648fa0494e74e7e7a5484d8b29f70ca14c3d
lovelyjean <lovely.jean@gmail.com>
2021-01-01 14:40:08 +0900

    calc average length

--- read.py
+++ read.py
@@ -1,13 +1,13 @@
 data = []
 count = 0
+length = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
+		length = length + (len(line))
 		count = count + 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('Total data:', len(data))
 
-print(data[0])
-print('-------------------------')
-print(data[1])
+print('Average length:', length / len(data))
