c8c53f7736f1284376e64346351a17226a5dfde2
lovelyjean <lovely.jean@gmail.com>
2021-01-01 14:49:29 +0900

    Add filtering

--- read.py
+++ read.py
@@ -11,3 +11,11 @@ with open('reviews.txt', 'r') as f:
 print('Total data:', len(data))
 
 print('Average length:', length / len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
